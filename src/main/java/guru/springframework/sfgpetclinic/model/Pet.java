98adcc379bc7d9230102088df4b07a9575f21a0c
haitham-b <haitham.bjanthala@gmail.com>
2021-01-15 13:22:22 +0100

    Adding POJOs

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Pet {
+
+    private PetType petType;
+    private Owner petOwner;
+    private LocalDate birthDate;
+
+
+}
