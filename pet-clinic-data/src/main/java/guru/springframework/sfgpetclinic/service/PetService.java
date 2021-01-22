1642bc269597c73d991f1e0cdd887de9a5b12203
haitham-b <haitham.bjanthala@gmail.com>
2021-01-22 10:26:03 +0100

    implement map based services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
@@ -2,7 +2,5 @@ package guru.springframework.sfgpetclinic.service;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long> {
 }
