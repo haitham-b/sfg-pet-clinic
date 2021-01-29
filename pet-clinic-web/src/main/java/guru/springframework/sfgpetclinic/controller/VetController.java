e28b9be24fb06f6ee51233d1510d792b80c2ef9a
haitham-b <haitham.bjanthala@gmail.com>
2021-01-29 17:00:56 +0100

    added vets list as thymeleaf table

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,14 +1,26 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/vet_index", "/vet_index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vet_index";
     }
 }
