7d49f4c54a2cd4f81d7441c877075fe9dd93215c
haitham-b <haitham.bjanthala@gmail.com>
2021-02-16 16:54:49 +0100

    adding speciality service interface and its map impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/SpecialtyServiceMap.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.SpecialtyService;
+import guru.springframework.sfgpetclinic.service.VetService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
