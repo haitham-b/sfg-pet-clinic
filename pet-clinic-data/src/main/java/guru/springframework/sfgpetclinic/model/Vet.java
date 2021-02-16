5269e10de277cf4e606c6fb091e265aa71137c64
haitham-b <haitham.bjanthala@gmail.com>
2021-02-16 16:47:52 +0100

    adding pets and assigning them to owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -2,9 +2,10 @@ package guru.springframework.sfgpetclinic.model;
 
 import lombok.Data;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
 public class Vet extends Person {
-    private Set<Specialty> specialties;
+    private Set<Specialty> specialties = new HashSet<>();
 }
