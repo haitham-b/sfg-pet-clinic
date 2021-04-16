38c3046fec85beae5029f4d7135913c9754319fb
haitham-b <haitham.bjanthala@gmail.com>
2021-04-16 15:20:56 +0200

    update profiles for JPA and Map impls.
    - also fix issue with stackoverflow by overriding Owner.toString and Owner.hashCode()

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,4 +20,14 @@ public class Owner extends Person {
 
     @OneToMany(cascade = CascadeType.ALL)
     private Set<Pet> pets = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return " { address: " + address + ", city: " + city + ", telephone: " + telephone + ", pets: " + Arrays.toString(pets.stream().map(BaseEntity::getId).toArray()) + " }";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
 }
