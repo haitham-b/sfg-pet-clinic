113a5dca82fbfeb60c8319db05f756b95081e616
haitham-b <haitham.bjanthala@gmail.com>
2021-04-16 13:49:10 +0200

    adding Specialty service using Spring Data JPA

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/SpecialtyRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/SpecialtyRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.sfgpetclinic.repository;
 import guru.springframework.sfgpetclinic.model.Specialty;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+    Set<Specialty> findAll();
 }
