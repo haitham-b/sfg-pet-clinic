a18a2d6db92e698e1f6f84cb75f3e52ac3874c55
haitham-b <haitham.bjanthala@gmail.com>
2021-04-16 14:52:31 +0200

    implemented jpa impl for Visit service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VisitRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VisitRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.sfgpetclinic.repository;
 import guru.springframework.sfgpetclinic.model.Visit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface VisitRepository extends CrudRepository<Visit, Long> {
+    Set<Visit> findAll();
 }
