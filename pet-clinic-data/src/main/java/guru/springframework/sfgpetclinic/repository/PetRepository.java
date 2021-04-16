470378767ac017053ad64926e874674ade60e2d9
haitham-b <haitham.bjanthala@gmail.com>
2021-04-16 13:44:54 +0200

    adding Pet service using Spring Data JPA

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/PetRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.sfgpetclinic.repository;
 import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface PetRepository extends CrudRepository<Pet, Long> {
+    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/PetTypeSDJpaService.java
@@ -3,11 +3,13 @@ package guru.springframework.sfgpetclinic.service.springdatajpa;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.repository.PetTypeRepository;
 import guru.springframework.sfgpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
     private final PetTypeRepository petTypeRepository;
 
